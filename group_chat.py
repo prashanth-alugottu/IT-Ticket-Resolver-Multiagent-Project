5d48c4da32c3fe65e3c69fc063309c0caa3f460d
Prashanth <chinnuprashanth868@gmail.com>
2026-04-26 12:53:41 +0530

    connecting the agents and communicating each other

---
+++ group_chat.py
@@ -0,0 +1,62 @@
+from autogen import GroupChat, GroupChatManager, UserProxyAgent
+from agents.classifier_agent import get_classifier_agent
+from agents.knowledge_base_agent import get_knowledge_base_agent
+from tools.knowledge_base_tool import search_similar_solution
+# from agents.notification_agnet import get_notification_agent
+
+# from agents.notification_agent import get_notification_agent
+# from tools.send_email import escalate_ticket_with_email
+from utils.llm_config import llm_config
+
+# Termination condition
+def is_termination_msg(message):
+    return isinstance(message, dict) and message.get("content", "").strip().upper() == "TERMINATE"
+
+
+# Create agents
+classifier = get_classifier_agent()
+kb_agent = get_knowledge_base_agent()
+# notification_agent = get_notification_agent()
+
+# Bind this manually to the agent
+# notification_agent.generate_reply = lambda messages, sender: escalate_ticket_with_email(
+    # issue=messages[0]["content"]
+# )
+
+
+# Create user agent
+user = UserProxyAgent(
+    name="User",
+    human_input_mode="TERMINATE",
+    code_execution_config={"use_docker": False},
+    is_termination_msg=is_termination_msg,
+)
+
+'''
+In AutoGen, GroupChat maintains the full message history between agents. 
+The messages=[] parameter initializes that history.
+'''
+
+# Create group chat with all agents
+groupchat = GroupChat(
+    agents=[user, classifier, kb_agent],
+    messages=[],
+    speaker_selection_method="Auto",
+    allow_repeat_speaker=True,
+    max_round=6
+)
+
+# Create group chat manager
+manager = GroupChatManager(
+    groupchat=groupchat,
+    llm_config=llm_config,
+    is_termination_msg=is_termination_msg,
+)
+
+
+if __name__=="__main__":
+    # Trigger conversation
+    user.initiate_chat(
+        recipient=manager,
+        message="Please resolve this issue: Outlook crashes every time I open it."
+    )
